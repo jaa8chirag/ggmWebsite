e4c3041e8d954b76a89468b8384c8ea040f5c830
Chirag Kumar <chiragchoudhary340@gmail.com>
2026-09-02 18:27:42 +0530

    chore: ensure GTM container active, let Vercel Edge manage canonical domain routing

--- next.config.ts
+++ next.config.ts
@@ -8,17 +8,6 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "www.ggmtechnologies.com",
-          },
-        ],
-        destination: "https://ggmtechnologies.com/:path*",
-        permanent: true,
-      },
-      {
         source: "/privacy-policy-2",
         destination: "/privacy-policy",
         permanent: true,
